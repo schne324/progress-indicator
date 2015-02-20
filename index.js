9ca980510f1e168ca51417cfebe57770fd1e1807
Harris Schneiderman <harris.schneiderman@deque.com>
2015-02-20 09:54:01 -0500

    Based on first SME feedback, adding an assertive example for testing...Also, updating the progress on blur of an input NOT every keydown...

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 
-var $inputs = $('.question input');
-var $progress = $('.progress');
+$.fn.progressIndicator = function () {
+  var $inputs = $(this).find('input');
+  var $progress = $(this).find('.progress');
+  var $submit = $(this).find('.submit');
 
-$inputs.on('keydown', function (keyVent) {
-  var wasFull = $progress.hasClass('full');
-  var wasHalf = $progress.hasClass('half');
-  setTimeout(function () {
-    var qa = checkInputs();
+  $inputs
+    .on('blur', function () {
+      var wasFull = $progress.hasClass('full');
+      var wasHalf = $progress.hasClass('half');
+      var qa = checkInputs($inputs);
 
-    if (qa < 1) {
-      if (wasHalf || wasFull) {
+      if (qa < 1) {
+        if (wasHalf || wasFull) {
+          $progress
+            .removeClass('full')
+            .removeClass('half')
+            .html('0% Complete')
+            .attr('aria-valuenow', '0');
+        }
+      } else if (qa === 1 && !wasHalf) {
         $progress
           .removeClass('full')
+          .addClass('half')
+          .html('50% Complete')
+          .attr('aria-valuenow', '50');
+      } else if (qa === 2 && !wasFull) {
+        $progress
           .removeClass('half')
-          .html('0% Complete')
-          .attr('aria-valuenow', '0');
+          .addClass('full')
+          .html('100% Complete')
+          .attr('aria-valuenow', '100');
       }
-    } else if (qa === 1 && !wasHalf) {
-      $progress
-        .removeClass('full')
-        .addClass('half')
-        .html('50% Complete')
-        .attr('aria-valuenow', '50');
-    } else if (qa === 2 && !wasFull) {
-      $progress
-        .removeClass('half')
-        .addClass('full')
-        .html('100% Complete')
-        .attr('aria-valuenow', '100');
+    })
+    .on('keydown', function (keyVent) {
+      if (keyVent.which === 13) { // ENTER
+        $submit.click();
+      }
+    });
+
+
+  $submit.on('click', function () {
+    var ariaVal = $progress.attr('aria-valuenow');
+
+    if (ariaVal === '0' || ariaVal === '50') {
+      alert('You\'re gonna have to tell me a little more about yourself');
+    } else {
+      alert('Thanks for the info!');
     }
-  }, 0);
+  });
 
-  // submit "form" (not actually a form) on ENTER
-  if (keyVent.which === 13) {
-    $('#submit').click();
-  }
-});
+  return this;
+}
 
-$('#submit').on('click', function () {
-  var ariaVal = $progress.attr('aria-valuenow');
-
-  if (ariaVal === '0' || ariaVal === '50') {
-    alert('You\'re gonna have to tell me a little more about yourself');
-  } else {
-    alert('Thanks for the info!');
-  }
-});
-
-function checkInputs() {
+function checkInputs($inputs) {
   var valInputs = [];
   $inputs.each(function () {
     if ($(this).val() !== '') {
@@ -58,3 +63,5 @@ function checkInputs() {
   return valInputs.length;
 }
 
+$('#polite').progressIndicator();
+$('#assertive').progressIndicator();
